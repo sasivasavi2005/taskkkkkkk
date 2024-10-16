039ed212c8755c55ea4b999fdb5ba3e76b1d50c1
sasivasavi2005 <129580912+sasivasavi2005@users.noreply.github.com>
2024-10-16 08:47:53 +0530

    Update task.js

--- task.js
+++ task.js
@@ -13,9 +13,9 @@ const themeToggleButton = document.getElementById("theme-toggle");
 function toggleTheme() {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode")) {
-    themeToggleButton.textContent = "Switch to Light Mode";
+    themeToggleButton.textContent = "Change Theme";
   } else {
-    themeToggleButton.textContent = "Switch to Dark Mode";
+    themeToggleButton.textContent = "Change Theme";
   }
 }
 themeToggleButton.addEventListener("click", toggleTheme);
